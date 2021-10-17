e7b7e3d4ce58d694001816bb499fbd0d60bea0f9
John Royal <34844819+john-royal@users.noreply.github.com>
2021-10-17 17:06:10 -0400

    chore: init

---
+++ app/src/main/java/com/codepath/johnroyal/thegram/ParseApplication.java
@@ -0,0 +1,19 @@
+package com.codepath.johnroyal.thegram;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("A0KIN2Nrb2XF3cx0jF1oO36EFloEpEmSymsIYQLP")
+                .clientKey("doAoGmwBZRkgPyyf81fYsPKzDWbzU1CWVMlYbTYY")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
---
+++ app/src/main/java/com/codepath/johnroyal/thegram/SignInActivity.java
@@ -0,0 +1,96 @@
+package com.codepath.johnroyal.thegram;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+    public static String TAG = "SignInActivity";
+    public final static int CREATE_ACCOUNT_REQUEST_CODE = 100;
+
+    EditText etUsername;
+    EditText etPassword;
+    Button btnSignIn;
+    Button btnCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        if (ParseUser.getCurrentUser() != null) {
+            launchMainActivity();
+            return;
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSignIn();
+            }
+        });
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCreateAccount();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CREATE_ACCOUNT_REQUEST_CODE && resultCode == RESULT_OK) {
+            assert(ParseUser.getCurrentUser() != null);
+            launchMainActivity();
+        }
+    }
+
+    private void handleSignIn() {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        Log.d(TAG, String.format("Signing in with username: %s", username));
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.i(TAG, String.format("Signed in successfully with username: %s", user.getUsername()));
+                    launchMainActivity();
+                } else {
+                    Log.e(TAG, String.format("Cannot sign in: %s", e.getLocalizedMessage()), e);
+                    Toast.makeText(SignInActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void launchCreateAccount() {
+        Intent i = new Intent(SignInActivity.this, CreateAccountActivity.class);
+        startActivityForResult(i, CREATE_ACCOUNT_REQUEST_CODE);
+    }
+
+    private void launchMainActivity() {
+        Intent i = new Intent(SignInActivity.this, NewPostActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
